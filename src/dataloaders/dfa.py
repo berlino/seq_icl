8dd624a313e080e846743a25da3c0069aaf9699b
Ekin Akyürek <akyurekekin@gmail.com>
2023-08-17 16:42:43 -0400

    update

--- src/dataloaders/dfa.py
+++ src/dataloaders/dfa.py
@@ -15,6 +15,7 @@ from src.dataloaders.base import SequenceDataset
 @dataclasses.dataclass
 class DFA:
     """Represents a DFA"""
+
     num_nodes: int
     alphabet: Tuple[str]
     transitions: Tuple[dict]
@@ -32,7 +33,7 @@ class DFA:
 
     def __call__(self, word: str):
         current_node = 0
-        for symbol in word:
+        for symbol in word.split():
             if symbol not in self.transitions[current_node]:
                 return False
             else:
@@ -46,8 +47,9 @@ class DFA:
         for _ in range(length):
             outgoing_symbols = list(self.transitions[current_node].keys())
             symbol = self.rng.choice(outgoing_symbols)
-            word += symbol
+            word += symbol + " "
             current_node = self.transitions[current_node][symbol]
+        word = word.rstrip()
         return word
 
 
@@ -80,13 +82,14 @@ class RandomDFASampler:
             )
             transition_nodes = self.rng.integers(self.num_nodes, size=num_transitions)
             transitions[node] = dict(zip(transition_symbols, transition_nodes))
-        dfa_rng = np.random.default_rng(self.rng.integers(0, 2 ** 32))
+        dfa_rng = np.random.default_rng(self.rng.integers(0, 2**32))
         return DFA(self.num_nodes, self.alphabet, tuple(transitions), dfa_rng)
 
 
 if __name__ == "__main__":
+
     def sample_usage():
-        dfa_sampler =  RandomDFASampler(4, ("a", "b", "c", "d"), 4, seed=2)
+        dfa_sampler = RandomDFASampler(4, ("a", "b", "c", "d"), 4, seed=2)
         dfa = dfa_sampler.sample()
         word = dfa.sample(length=10)
         print(word)
@@ -94,6 +97,7 @@ if __name__ == "__main__":
         print(word)
 
     sample_usage()
+
 
 class Vocab:
     """Custom vocab."""
@@ -132,6 +136,8 @@ class Vocab:
     def get_vocab(self, id: int):
         return self.vocab[id]
 
+
+
     def __len__(self):
         return len(self.vocab)
 
@@ -142,13 +148,18 @@ class Tokenizer:
     def __init__(self, vocab: Vocab):
         self.vocab = vocab
 
-    def tokenize(self, text: str, return_tensor: bool = False, mask_input: bool = False):
+    def tokenize(
+        self, text: str, return_tensor: bool = False, mask_input: bool = False
+    ):
         input_ids = [self.vocab.get_id(t) for t in text.split()]
-        labels = input_ids
+
+        labels = input_ids[1:]
+        input_ids = input_ids[:-1]
 
         if return_tensor:
             input_ids = torch.LongTensor(input_ids)
             labels = torch.LongTensor(labels)
+
         return {
             "input_ids": input_ids,
             "labels": labels,
@@ -156,6 +167,20 @@ class Tokenizer:
 
     def decode(self, ids: list):
         return " ".join([self.vocab.get_vocab(id) for id in ids])
+
+
+class SimpleDataset(Dataset):
+    def __init__(self, examples, dfas):
+        super().__init__()
+        self.inputs = examples[0]
+        self.targets = examples[1]
+        self.dfas = dfas
+
+    def __len__(self):
+        return len(self.inputs)
+
+    def __getitem__(self, idx):
+        return self.inputs[idx], self.targets[idx], self.dfas[idx]
 
 
 class ICLDFADataModule(SequenceDataset):
@@ -200,9 +225,7 @@ class ICLDFADataModule(SequenceDataset):
 
         special_vocabs = {"seperator": "|", "noop": "."}
         self.special_vocabs = special_vocabs
-        self.vocab = Vocab(
-            vocab_size-2, special_vocabs=special_vocabs
-        )
+        self.vocab = Vocab(vocab_size - 2, special_vocabs=special_vocabs)
         self.tokenizer = Tokenizer(self.vocab)
 
     def generate_example(self, dfa: DFA, num_examples: int):
@@ -210,131 +233,94 @@ class ICLDFADataModule(SequenceDataset):
         for _ in range(num_examples):
             length = self.rng.integers(self.max_len_per_example)
             word = dfa.sample(length=length)
-            example += word + "|"
-        example = example[:-1]
+            example += word + " | "
+        example = example[:-3]
         if len(example) > self.input_seq_len:
-            example = example[:self.input_seq_len]
-        example = " ".join(list(example))  # separate chars with space
+            example = example[: self.input_seq_len]
+        # example = " ".join(list(example))  # separate chars with space
+
         return self.tokenizer.tokenize(example, return_tensor=True)
 
     def setup(self, stage=None):
-        train_tensor = test_tensor = None
-        if self.data_dir is not None:
-            try:
-                train_tensor = torch.load(
-                    os.path.join(
-                        self.data_dir,
-                        f"train_{self.copy_method}_{self.num_examples}_{self.vocab_size}_{self.input_seq_len}.pt",
-                    )
-                )
-                test_tensor = torch.load(
-                    os.path.join(
-                        self.data_dir,
-                        f"test_{self.copy_method}_{self.num_examples}_{self.vocab_size}_{self.input_seq_len}.pt",
-                    )
-                )
-            except:
-                pass
+        if hasattr(self, "dataset"):
+            return
 
-        if train_tensor is None or test_tensor is None:
-            if hasattr(self, "dataset"):
-                return
-            self.rng = np.random.default_rng(self.seed)
+        self.rng = np.random.default_rng(self.seed)
 
-            DFAs = set([])
-            for _ in range(self.num_examples * 10):
-                num_nodes = self.rng.integers(2, self.max_num_nodes)
-                alphabet = self.rng.choice(self.vocab_size, size=num_nodes, replace=False)
-                alphabet = tuple((str(a) for a in alphabet))
-                sampler = RandomDFASampler(
-                        num_nodes,
-                        alphabet,
-                        min(self.max_outgoing_edges, len(alphabet)),
-                    )
-                sampler.rng = self.rng
-                dfa = sampler.sample()
-                DFAs.add(dfa)
-                if len(DFAs) >= self.num_examples + self.num_test_examples:
-                    break
-            DFAs = list(DFAs)
-            self.rng.shuffle(DFAs)
-            if len(DFAs) < self.num_examples + self.num_test_examples:
-                print(
-                    "Warning: not enough unique DFAs generated. Using all generated"
-                    " DFAs."
-                )
-                # scale back
-                self.num_examples = (len(DFAs) * self.num_examples) // (
-                    self.num_examples + self.num_test_examples
-                )
-                self.num_test_examples = len(DFAs) - self.num_examples
-                print(
-                    f"New num_examples: {self.num_examples}, new num_test_examples:"
-                    f" {self.num_test_examples}"
-                )
-
-            train_DFAs = DFAs[: self.num_examples]
-            test_DFAs = DFAs[
-                self.num_examples : self.num_examples + self.num_test_examples
-            ]
-
-            all_examples = []
-            for DFAs in (train_DFAs, test_DFAs):
-                examples = []
-                for dfa in DFAs:
-                    num_samples = self.rng.integers(
-                        self.min_num_in_context_examples,
-                        self.max_num_in_context_examples,
-                    )
-                    example = self.generate_example(dfa, num_samples)[
-                        "input_ids"
-                    ].tolist()
-                    examples.append(example)
-
-                self.rng.shuffle(examples)
-                # pad examples to same length
-                examples = torch.nn.utils.rnn.pad_sequence(
-                    [torch.LongTensor(example) for example in examples],
-                    batch_first=True,
-                    padding_value=self.vocab.get_id(self.vocab.noop),
-                )
-
-                all_examples.append(examples)
-
-            # all_examples = torch.concat(all_examples)
-            train_tensor = torch.stack(
-                [
-                    torch.stack([example[:-1], example[1:]])
-                    for example in all_examples[0]
-                ]
+        DFAs = set([])
+        for _ in range(self.num_examples * 10):
+            num_nodes = self.rng.integers(2, self.max_num_nodes)
+            alphabet = self.rng.choice(self.vocab_size-2, size=num_nodes, replace=False)
+            alphabet = tuple((str(a) for a in alphabet))
+            sampler = RandomDFASampler(
+                num_nodes,
+                alphabet,
+                min(self.max_outgoing_edges, len(alphabet)),
             )
-            test_tensor = torch.stack(
-                [
-                    torch.stack([example[:-1], example[1:]])
-                    for example in all_examples[1]
-                ]
+            sampler.rng = self.rng
+            dfa = sampler.sample()
+            DFAs.add(dfa)
+            if len(DFAs) >= self.num_examples + self.num_test_examples:
+                break
+
+        DFAs = list(DFAs)
+        self.rng.shuffle(DFAs)
+
+        if len(DFAs) < self.num_examples + self.num_test_examples:
+            print(
+                "Warning: not enough unique DFAs generated. Using all generated DFAs."
+            )
+            # scale back
+            self.num_examples = (len(DFAs) * self.num_examples) // (
+                self.num_examples + self.num_test_examples
+            )
+            self.num_test_examples = len(DFAs) - self.num_examples
+            print(
+                f"New num_examples: {self.num_examples}, new num_test_examples:"
+                f" {self.num_test_examples}"
             )
 
-            if self.data_dir is not None:
-                torch.save(
-                    train_tensor,
-                    os.path.join(
-                        self.data_dir,
-                        f"train_{self.copy_method}_{self.num_examples}_{self.vocab_size}_{self.input_seq_len}.pt",
-                    ),
+        DFAs = {"train": DFAs[: self.num_examples], "test": DFAs[self.num_examples :]}
+
+        examples = {"train": [], "test": []}
+
+        for split, dfas in DFAs.items():
+            split_examples = []
+            for dfa in dfas:
+                num_samples = self.rng.integers(
+                    self.min_num_in_context_examples,
+                    self.max_num_in_context_examples,
                 )
-                torch.save(
-                    test_tensor,
-                    os.path.join(
-                        self.data_dir,
-                        f"test_{self.copy_method}_{self.num_examples}_{self.vocab_size}_{self.input_seq_len}.pt",
-                    ),
-                )
+                example = self.generate_example(dfa, num_samples)
+                input, output = example["input_ids"], example["labels"]
+
+                split_examples.append((input, output))
+
+            # pad examples to same length
+            example_inputs = torch.nn.utils.rnn.pad_sequence(
+                [example[0] for example in split_examples],
+                batch_first=True,
+                padding_value=self.vocab.get_id(self.vocab.noop),
+            )
+
+            example_outputs = torch.nn.utils.rnn.pad_sequence(
+                [example[1] for example in split_examples],
+                batch_first=True,
+                padding_value=-100,
+            )
+
+            examples[split] = (example_inputs, example_outputs)
 
         self.dataset = {
-            "train": TensorDataset(train_tensor[:, 0, :], train_tensor[:, 1, :]),
-            "test": TensorDataset(test_tensor[:, 0, :], test_tensor[:, 1, :]),
+            "train": SimpleDataset(examples=examples["train"], dfas=DFAs["train"]),
+            "test": SimpleDataset(examples=examples["test"], dfas=DFAs["test"]),
         }
+
+    def _collate_fn(self, batch):
+        xs, ys, dfas = zip(*batch)
+        xs = torch.stack(xs)
+        ys = torch.stack(ys)
+        return xs, ys, dfas
 
     def train_dataloader(self, *args, **kwargs):
         return self._data_loader(self.dataset["train"], shuffle=True)
@@ -351,6 +337,7 @@ class ICLDFADataModule(SequenceDataset):
             batch_size=self.batch_size,
             num_workers=10,
             shuffle=shuffle,
+            collate_fn=self._collate_fn,
             persistent_workers=True,
         )
 
