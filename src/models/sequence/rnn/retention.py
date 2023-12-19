791af69e88e92c673d9faa8ae640d66f150499e0
berlino <bailin.wang28@gmail.com>
2023-12-19 17:11:33 -0500

    update induction head

--- src/models/sequence/rnn/retention.py
+++ src/models/sequence/rnn/retention.py
@@ -64,19 +64,28 @@ class MultiScaleRetention(nn.Module):
         assert (pt_residual and ih_residual) is False, "only one residual connection is allowed"
 
         # reset the residual connection if it is not the second-to-last layer
-        if self.layer_idx != 2:
-            self.pt_residual = False
+        if self.layer_idx  == 1:
+            self.pt_residual = True
             self.ih_residual = False
+        elif self.layer_idx == 2:
+            self.pt_residual = False
+            self.ih_residual = True
+        else:
+            assert (pt_residual or ih_residual) is False, "only one residual connection is allowed"
 
         if self.pt_residual:
             self.token_shift = nn.ZeroPad2d((0, 0, 1, -1))
-            self.t0 = torch.nn.Parameter(torch.zeros(self.embed_dim))
-            self.t1 = torch.nn.Parameter(torch.ones(self.embed_dim))
+            # self.t0 = torch.nn.Parameter(torch.zeros(self.embed_dim))
+            # self.t1 = torch.nn.Parameter(torch.ones(self.embed_dim))
+            self.t0 = torch.nn.Linear(2 * self.embed_dim, 2 * self.embed_dim)
+            self.t1 = torch.nn.Linear(2 * self.embed_dim, 2 * self.embed_dim)
         elif self.ih_residual:
-            self.t0 = torch.nn.Parameter(torch.zeros(self.embed_dim))
-            self.t1 = torch.nn.Parameter(torch.ones(self.embed_dim))
+            # self.t0 = torch.nn.Parameter(torch.zeros(self.embed_dim))
+            # self.t1 = torch.nn.Parameter(torch.ones(self.embed_dim))
+            self.t0 = torch.nn.Linear(2 * self.embed_dim, 2 * self.embed_dim)
+            self.t1 = torch.nn.Linear(2 * self.embed_dim, 2 * self.embed_dim)
 
-    def parallel_forward(self, qr, kr, v, mask):
+    def inner_forward(self, qr, kr, v, mask):
         bsz, tgt_len, embed_dim = v.size()
         vr = v.view(bsz, tgt_len, self.num_heads, self.head_dim).transpose(1, 2)
 
@@ -87,7 +96,6 @@ class MultiScaleRetention(nn.Module):
         output = torch.matmul(qk_mat, vr)
         output = output.transpose(1, 2)
         return output
-    
         
 
     def forward(self, x, return_attention=False, input_ids=None):
@@ -106,20 +114,28 @@ class MultiScaleRetention(nn.Module):
         qr = theta_shift(q, sin, cos)
         kr = theta_shift(k, sin, cos)
 
-        output = self.parallel_forward(qr, kr, v, inner_mask)
-
-        output = self.group_norm(output).reshape(bsz, tgt_len, self.head_dim * self.num_heads)
-        output = self.gate_fn(g) * output
-        output = self.out_proj(output)
+        output = self.inner_forward(qr, kr, v, inner_mask)
+        output = output.reshape(bsz, tgt_len, self.head_dim * self.num_heads)
 
         if self.pt_residual:
-            h0 = self.token_shift(output)
+            # h0 = self.token_shift(output)
+            h0 = self.token_shift(v)
             h1 = output
-            output = self.t0 * h0 + self.t1 * h1
+            # output = self.t0 * h0 + self.t1 * h1
+            # breakpoint()
+            output = self.t0(h0) + self.t1(h1)
         elif self.ih_residual:
-            h0 = induction_head(input_ids, output)
+            # h0 = induction_head(input_ids, output)
+            h0 = induction_head(input_ids, v)
             h1 = output
-            output = self.t0 * h0 + self.t1 * h1
+            # output = self.t0 * h0 + self.t1 * h1
+            output = self.t0(h0) + self.t1(h1)
+
+        assert self.num_heads == 1
+        output = self.group_norm(output)
+
+        output = self.gate_fn(g) * output
+        output = self.out_proj(output)
 
         # attention output is not supported from now
         if return_attention:
@@ -173,20 +189,23 @@ class RetNetRelPos(nn.Module):
 
         return retention_rel_pos
 
-def induction_head(x, hidden_state):
+def induction_head(x, hidden_state, shift_right=True):
     """
     Args:
         x: bsz x input_len
         hidden_state: bsz x input_len x d_model
+        shift_right: use the second token from the bigram
     Output:
         bsz x input_len x d_model
     """
     bsz, seq_len = x.shape
 
-    # import pbd; pdb.set_trace()
+    # bsz x L x L
     same_mask = x[:, :, None] == x[:, None, :]
     causal_mask = torch.tril(torch.ones(seq_len, seq_len, dtype=torch.bool, device=x.device), diagonal=-1)
     ih_mask = torch.logical_and(same_mask, causal_mask).float()
+    if shift_right:
+        ih_mask = F.pad(ih_mask, (-1, 1), "constant", False)
     ih_mask_norm = ih_mask / ih_mask.sum(dim=2, keepdim=True)
     ih_mask_norm = torch.nan_to_num(ih_mask_norm, 0)
     output = torch.einsum("bmn,bnz->bmz", ih_mask_norm, hidden_state)
